0f84e491cc1951c190f1cde39b6a05d36d69e56b
Marc Fairbrother <fairbrother.marc@gmail.com>
2019-12-11 18:48:24 +0100

    Footer mods

---
+++ components/HomeFooter.vue
@@ -0,0 +1,39 @@
+<template>
+  <footer>
+    <p>
+      <nuxt-link to="/credits">Crédits</nuxt-link>
+    </p>
+    <p>
+      MarcFairbrother
+      <span class="flip">&copy;</span>
+      2019
+    </p>
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+footer {
+  align-items: center;
+  background: var(--dark);
+  color: var(--light);
+  display: flex;
+  font-size: 0.8rem;
+  height: 40px;
+  justify-content: space-between;
+  margin: 0;
+  padding: 0 var(--fll-margin);
+  @include breakpoint($desktop-width) {
+    font-size: 0.6rem;
+    grid-column: 1/3;
+    grid-row: 2;
+    padding-bottom: 0;
+  }
+  & a {
+    color: var(--light);
+  }
+}
+.flip {
+  display: inline-block;
+  transform: rotateZ(180deg);
+}
+</style>
