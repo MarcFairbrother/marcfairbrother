832ef48014b6356c06386899aeeb380a167b63a2
Marc Fairbrother <fairbrother.marc@gmail.com>
2020-01-19 15:27:55 +0100

    Set up basic carousel logic in photography section

--- pages/photography/index.vue
+++ pages/photography/index.vue
@@ -1,12 +1,28 @@
 <template>
-  <main>
-    <h2>Photgraphy</h2>
-    <ul v-if="photos.length > 0">
-      <li v-for="photo in photos" :key="photo.public_id">
-        <img :src="cloudinarySrc(photo.public_id)" alt />
-      </li>
-    </ul>
-    <p v-else>Could not load photos</p>
+  <main class="photography">
+    <header class="photography__header">
+      <h2>[ ◎°] photography</h2>
+    </header>
+    <article class="photography__content">
+      <ul v-if="photos.length > 0" class="photography__content__list">
+        <li
+          v-for="(photo, i) in photos"
+          v-bind:data-target="i"
+          :key="i"
+          @click="showCarousel($event)"
+          class="photography__content__list__item"
+        >
+          <img :src="cloudinarySrc(photo.public_id)" v-bind:data-target="i" alt />
+        </li>
+      </ul>
+      <p v-else>Could not load photos</p>
+    </article>
+    <article v-show="carouselIsVisible && currentImgPublic" class="photography__carousel">
+      <img :src="cloudinarySrc(currentImgPublic)" alt class="photography__carousel__image" />
+      <p @click="showPrevious()" class="photography__carousel__previous">Previous</p>
+      <p @click="showNext()" class="photography__carousel__next">Next</p>
+      <p @click="carouselIsVisible = !carouselIsVisible" class="photography__carousel__close">Close</p>
+    </article>
   </main>
 </template>
 
@@ -27,12 +43,38 @@ const photosRequest = cloudinaryCore.url("marc", {
 export default {
   data() {
     return {
-      photos: []
+      photos: [],
+      carouselIsVisible: false,
+      currentImgPublic: "",
+      currentImgId: ""
     };
   },
   methods: {
     cloudinarySrc: function(publicId) {
       return cloudinaryCore.url(publicId);
+    },
+    showCarousel: function(e) {
+      this.carouselIsVisible = !this.carouselIsVisible;
+      this.currentImgId = e.target.getAttribute("data-target");
+      this.currentImgPublic = this.photos[this.currentImgId].public_id;
+    },
+    showPrevious: function() {
+      if (this.currentImgId > 0) {
+        this.currentImgId = this.currentImgId - 1;
+        this.currentImgPublic = this.photos[this.currentImgId].public_id;
+      } else {
+        this.currentImgId = this.photos.length - 1;
+        this.currentImgPublic = this.photos[this.currentImgId].public_id;
+      }
+    },
+    showNext: function() {
+      if (this.currentImgId < this.photos.length - 1) {
+        this.currentImgId = this.currentImgId + 1;
+        this.currentImgPublic = this.photos[this.currentImgId].public_id;
+      } else {
+        this.currentImgId = 0;
+        this.currentImgPublic = this.photos[this.currentImgId].public_id;
+      }
     }
   },
   mounted() {
@@ -44,7 +86,110 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-main {
-  height: 300vh;
+.photography {
+  @include breakpoint($desktop-width) {
+    border: solid var(--hlf-margin) var(--clr-5);
+    border-top: none;
+  }
+  &__header {
+    @include breakpoint($desktop-width) {
+      background: var(--light);
+      border-top: solid var(--hlf-margin) var(--clr-5);
+      padding-bottom: var(--hlf-margin);
+      padding-top: var(--hlf-margin);
+      position: sticky;
+      top: 0;
+      z-index: 5;
+    }
+    @include breakpoint($large-width) {
+      padding-top: var(--fll-margin);
+    }
+    & h2 {
+      background: var(--clr-5);
+      color: var(--light);
+      font-size: 2.5rem;
+      margin-bottom: var(--hlf-margin);
+      padding: var(--hlf-margin);
+      @include breakpoint($desktop-width) {
+        margin: 0 var(--hlf-margin);
+      }
+      @include breakpoint($large-width) {
+        font-size: 2rem;
+        line-height: calc(2 * var(--fll-margin));
+        margin: 0 var(--fll-margin) var(--hlf-margin);
+      }
+    }
+  }
+  &__content {
+    padding: 0 var(--hlf-margin) var(--hlf-margin);
+    @include breakpoint($large-width) {
+      padding: 0 var(--fll-margin) var(--fll-margin);
+    }
+    &__list {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      grid-gap: var(--hlf-margin);
+      @include breakpoint($desktop-width) {
+        grid-template-columns: repeat(3, 1fr);
+      }
+      @include breakpoint($large-width) {
+        grid-gap: var(--fll-margin);
+      }
+      &__item {
+        display: block;
+        padding-top: 100%;
+        position: relative;
+        width: 100%;
+        & > img {
+          height: 100%;
+          left: 0;
+          object-fit: cover;
+          position: absolute;
+          top: 0;
+          width: 100%;
+          &:hover {
+            cursor: pointer;
+          }
+        }
+      }
+    }
+  }
+  &__carousel {
+    background: var(--dark);
+    bottom: 0;
+    color: var(--light);
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    left: 0;
+    position: absolute;
+    right: 0;
+    top: 0;
+    z-index: 10;
+    &__image {
+      grid-column: 1/4;
+      grid-row: 1;
+    }
+    &__close {
+      grid-column: 2/3;
+      grid-row: 2;
+      &:hover {
+        cursor: pointer;
+      }
+    }
+    &__previous {
+      grid-column: 1/2;
+      grid-row: 2;
+      &:hover {
+        cursor: pointer;
+      }
+    }
+    &__next {
+      grid-column: 3/4;
+      grid-row: 2;
+      &:hover {
+        cursor: pointer;
+      }
+    }
+  }
 }
 </style>
