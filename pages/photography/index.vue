e4ce05cda50d6a07115bc3c86ab05b0fc6cacca1
Marc Fairbrother <fairbrother.marc@gmail.com>
2020-01-19 12:29:52 +0100

    Fetch Cloudinary data with axios

--- pages/photography/index.vue
+++ pages/photography/index.vue
@@ -1,40 +1,44 @@
 <template>
   <main>
     <h2>Photgraphy</h2>
-    <p>{{ test }}</p>
-    <p>{{ test2 }}</p>
-    <img :src="cloudinarySrc" alt />
-    <p>{{ cloudinaryTst }}</p>
+    <ul v-if="photos.length > 0">
+      <li v-for="photo in photos" :key="photo.public_id">
+        <img :src="cloudinarySrc(photo.public_id)" alt />
+      </li>
+    </ul>
+    <p v-else>Could not load photos</p>
   </main>
 </template>
 
 <script>
 import cloudinary from "cloudinary-core";
+import axios from "axios";
 
 const cloudinaryCore = new cloudinary.Cloudinary({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   secure: true
 });
 
+const photosRequest = cloudinaryCore.url("marc", {
+  format: "json",
+  type: "list"
+});
+
 export default {
   data() {
     return {
-      test: process.env.TEST_VARIABLE,
-      test2: "text"
+      photos: []
     };
   },
-  computed: {
-    cloudinarySrc() {
-      return cloudinaryCore.url(
-        "marcfairbrother/23_portrait_guggenheim_avo6cf"
-      );
-    },
-    cloudinaryTst() {
-      return cloudinaryCore.url("marc", {
-        format: "json",
-        type: "list"
-      });
+  methods: {
+    cloudinarySrc: function(publicId) {
+      return cloudinaryCore.url(publicId);
     }
+  },
+  mounted() {
+    axios.get(photosRequest).then(response => {
+      this.photos = response.data.resources;
+    });
   }
 };
 </script>
