5f678dd36cac9c761d8936b79203439f10a6d110
Marc Fairbrother <fairbrother.marc@gmail.com>
2020-01-29 15:58:13 +0100

    Finish work on resume section

--- pages/resume/index.vue
+++ pages/resume/index.vue
@@ -4,17 +4,37 @@
       <h2>Git Log --cv</h2>
       <nav>
         <ul class="resume__menu">
-          <li class="resume__menu__item" data-target="overview">
+          <li
+            class="resume__menu__item resume__menu__item--button"
+            data-target="overview"
+          >
             <button @click="navScroll($event)">Profil</button>
           </li>
-          <li class="resume__menu__item" data-target="work">
+          <li
+            class="resume__menu__item resume__menu__item--button"
+            data-target="work"
+          >
             <button @click="navScroll($event)">Expérience</button>
           </li>
-          <li class="resume__menu__item" data-target="studies">
+          <li
+            class="resume__menu__item resume__menu__item--button"
+            data-target="studies"
+          >
             <button @click="navScroll($event)">Formations</button>
           </li>
-          <li class="resume__menu__item" data-target="hobbies">
+          <li
+            class="resume__menu__item resume__menu__item--button"
+            data-target="hobbies"
+          >
             <button @click="navScroll($event)">Loisirs</button>
+          </li>
+          <li class="resume__menu__item resume__menu__item--link">
+            <a
+              href="https://www.linkedin.com/in/marcfairbrother"
+              target="_blank"
+              title="Marc's LinkedIn Profile"
+              >Contact</a
+            >
           </li>
         </ul>
       </nav>
@@ -256,131 +276,74 @@
       >
         <h3>Loisirs</h3>
         <ul
-          class="resume__content__section__list resume__content__section__list--hobbies"
+          class="resume__content__section__list resume__content__section__list--work"
         >
           <li>
-            <h4>10 Films</h4>
-            <ul
-              class="resume__content__section__list resume__content__section__list--sublist"
-            >
-              <li>
-                <p><em>Le Goût du saké</em> (Yasujirō Ozu, 1962)</p>
-              </li>
-              <li>
-                <p><em>La Poursuite infernale</em> (John Ford, 1946)</p>
-              </li>
-              <li>
-                <p><em>Possession</em> (Andrzej Żuławski, 1981)</p>
-              </li>
-              <li>
-                <p><em>Les Seigneurs</em> (Philip Kaufman, 1979)</p>
-              </li>
-              <li>
-                <p><em>Le Jour se lève</em> (Marcel Carné, 1939)</p>
-              </li>
-              <li>
-                <p><em>Prince des ténèbres</em> (John Carpenter, 1987)</p>
-              </li>
-              <li>
-                <p>
-                  <em>Le Colonel Blimp</em> (Michael Powell &amp; Emeric
-                  Pressburger, 1953)
-                </p>
-              </li>
-              <li>
-                <p><em>Eros + Massacre</em> (Kiju Yoshida, 1969)</p>
-              </li>
-              <li>
-                <p><em>L'Esprit de la ruche</em> (Víctor Erice, 1977)</p>
-              </li>
-              <li>
-                <p>
-                  <em>Apportez moi la tête d'Alfredo Garcia</em> (Sam Peckinpah,
-                  1974)
-                </p>
-              </li>
-            </ul>
+            <h4>Cinéma</h4>
+            <p>
+              Figuration sur plusieurs
+              <strong>tournages pour le cinéma</strong>&nbsp;:
+              <em>L'Ex-femme de ma vie</em> (Josiane Balasko, 2004),
+              <em>Le Temps qui reste</em> (François Ozon, 2005),
+              <em>Jean-Philippe</em> (Laurent Tuel, 2006),
+              <em>Mon meilleur ami</em> (Patrice Leconte, 2006), et pour la
+              télévision&nbsp;: <em>PJ</em> (diffusion sur France 2 de 1997 à
+              2009).
+            </p>
+            <p>
+              <strong>Création du blog Cinésthésies en 2012</strong> (dernière
+              refonte datant de 2014), un projet me permettant d'écrire sur un
+              sujet qui me passionne et d'aprendre les bases du développement
+              web (site créé avec Wordpress(.org) pour lequel j'ai développé un
+              thème from scratch et plusieurs plugins pour gérer des custom post
+              types). Je n'ai malheureusement pas eu le temps de continuer à
+              alimenter ce site en contenus depuis quelques années.
+            </p>
+            <p>
+              <strong>Rédacion d'articles et de critiques</strong> de films
+              et/ou d'éditions vidéo parus dans la revue <em>Metaluna</em> entre
+              avril et juin 2013.
+            </p>
+            <p>
+              Je reste toujours aussi passionné par le cinéma, notamment le
+              cinéma classique et d'exploitation, et je ne manque pas d'assister
+              à L'Étrange festival et au PIFFF chaque année. J'ai également de
+              <strong>nouveaux projets de sites</strong> sur lesquels je
+              travaille activement pendant mon temps libre.
+            </p>
           </li>
           <li>
-            <h4>10 Livres</h4>
-            <ul
-              class="resume__content__section__list resume__content__section__list--sublist"
-            >
-              <li>
-                <p><em>La Chance d'Omensetter</em> (William H. Gass, 1966)</p>
-              </li>
-              <li>
-                <p><em>L'Homme soleil</em> (John Gardner, 1972)</p>
-              </li>
-              <li>
-                <p><em>Le Loup des steppes</em> (Hermann Hesse, 1927)</p>
-              </li>
-              <li>
-                <p><em>Moby Dick</em> (Herman Melville, 1851)</p>
-              </li>
-              <li>
-                <p><em>Méridien de sang</em> (Cormac McCarthy, 1985)</p>
-              </li>
-              <li>
-                <p>
-                  <em>L'Education sentimentale</em> (Gustave Flaubert, 1869)
-                </p>
-              </li>
-              <li>
-                <p><em>Neuromancien</em> (William Gibson, 1984)</p>
-              </li>
-              <li>
-                <p></p>
-              </li>
-              <li>
-                <p></p>
-              </li>
-              <li>
-                <p></p>
-              </li>
-            </ul>
+            <h4>Photographie</h4>
+            <p>
+              Travail en argentique dans le cadre de stages universitaires
+              (prise de vue, développement noir &amp; blanc, tirage).
+            </p>
+            <p>
+              Je retrouve aujourd'hui de l'enthousiasme pour la photo numérique.
+              Débutant, je travaille pour l'instant avec un Sony RX100 III et le
+              logiciel open source RawTherapee dont j'ai une assez bonne
+              connaissance.
+            </p>
           </li>
           <li>
-            <h4>10 Albums</h4>
-            <ul
-              class="resume__content__section__list resume__content__section__list--sublist"
-            >
-              <li>
-                <p><em>F#A#∞</em> (Godspeed You! Black Emperor, 1997)</p>
-              </li>
-              <li>
-                <p><em>Basket of Light</em> (Pentangle, 1969)</p>
-              </li>
-              <li>
-                <p><em>Black Sabbath</em> (Black Sabbath, 1970)</p>
-              </li>
-              <li>
-                <p><em>Giant Steps</em> (John Coltrane, 1960)</p>
-              </li>
-              <li>
-                <p>
-                  <em>Live at the Old Quarter, Houston, Texas</em> (Townes Van
-                  Zandt, 1977)
-                </p>
-              </li>
-              <li>
-                <p>
-                  <em>New Skin for the Old Ceremony</em> (Leonard Cohen, 1974)
-                </p>
-              </li>
-              <li>
-                <p><em>Ecophony Rinne</em> (Geinoh Yamashirogumi, 1986)</p>
-              </li>
-              <li>
-                <p><em>White Chalk</em> (PJ Harvey, 2007)</p>
-              </li>
-              <li>
-                <p><em>Odessey and Oracle</em> (The Zombies, 1968)</p>
-              </li>
-              <li>
-                <p><em>666.667 Club</em> (Noir Désir, 1996)</p>
-              </li>
-            </ul>
+            <h4>Littérature</h4>
+            <p>
+              Je possède une excellente connaissance de la littérature classique
+              française, britannique, européenne et nord-américaine.
+              <em>Méridien de sang</em> de Cormac McCarthy,
+              <em>L'Homme soleil</em> de John Gardner,
+              <em>Le Loup des steppes</em> de Herman Hesse et
+              <em>La Chance d'Omensetter</em> de William H. Gass figurent parmi
+              mes romans préférés.
+            </p>
+            <p>
+              J'aime énormément la littérature de genre aussi, particulièrement
+              les premiers récits de William Gibson (<em>Gravé sur Chrome</em>,
+              <em>Neuromancien</em>, <em>Comte Zéro</em> et
+              <em>Mona Lisa s'éclate</em>), les nouvelles d'H.P. Lovecraft et le
+              premier recueil de Nathan Ballingrud,
+              <em>North American Lake Monsters</em>.
+            </p>
           </li>
         </ul>
       </section>
@@ -528,6 +491,8 @@ export default {
     grid-template-columns: 1fr;
   }
   &__header {
+    display: grid;
+    grid-template-columns: max-content 1fr;
     @include mobile {
       margin-bottom: var(--hlf-margin);
     }
@@ -535,7 +500,6 @@ export default {
       background: var(--light);
       border-top: solid var(--hlf-margin) var(--clr-3);
       display: grid;
-      grid-template-columns: max-content 1fr;
       padding: var(--hlf-margin);
       position: sticky;
       top: 0;
@@ -549,6 +513,7 @@ export default {
     & h2 {
       background: var(--clr-3);
       color: var(--light);
+      display: inline-block;
       font-size: 2.5rem;
       padding: var(--hlf-margin);
       text-transform: lowercase;
@@ -563,40 +528,61 @@ export default {
     & nav {
       background: var(--clr-3);
       color: var(--light);
-      @include mobile {
-        display: none;
-      }
       & .resume__menu {
-        @include breakpoint($desktop-width) {
-          align-items: center;
-          display: flex;
-          height: 100%;
-          justify-content: flex-end;
-          &__item {
-            margin-right: var(--hlf-margin);
-            position: relative;
-            @include breakpoint($large-width) {
-              margin-left: var(--hlf-margin);
-              &:last-of-type {
-                margin-right: var(--fll-margin);
-              }
+        align-items: center;
+        display: flex;
+        height: 100%;
+        justify-content: flex-end;
+        &__item {
+          line-height: 2;
+          margin-right: var(--hlf-margin);
+          position: relative;
+          @include breakpoint($large-width) {
+            margin-left: var(--hlf-margin);
+            &:last-of-type {
+              margin-right: var(--fll-margin);
+            }
+            &::before {
+              background-color: var(--light);
+              bottom: -3px;
+              border-radius: 50%;
+              content: "";
+              display: block;
+              height: 6px;
+              left: calc(50% - 3px);
+              opacity: 0;
+              position: absolute;
+              transition: opacity 0.25s ease-in-out;
+              width: 6px;
+            }
+            &--active {
               &::before {
-                background-color: var(--light);
-                bottom: -20px;
-                border-radius: 50%;
-                content: "";
-                display: block;
-                height: 6px;
-                left: calc(50% - 3px);
-                opacity: 0;
-                position: absolute;
-                transition: opacity 0.25s ease-in-out;
-                width: 6px;
+                opacity: 1;
               }
-              &--active {
-                &::before {
-                  opacity: 1;
-                }
+            }
+          }
+          &--button {
+            display: none;
+            @include breakpoint($large-width) {
+              display: initial;
+            }
+          }
+          &--link {
+            & a {
+              align-items: center;
+              border-bottom: solid 2px var(--light);
+              color: var(--light);
+              display: flex;
+              &::before {
+                background-image: var(--linkedin-white);
+                background-position: center;
+                background-repeat: no-repeat;
+                background-size: 15px;
+                content: "";
+                height: 20px;
+                margin-right: var(--qtr-margin);
+                margin-top: -2px;
+                width: 20px;
               }
             }
           }
@@ -791,14 +777,23 @@ export default {
         }
       }
       &--hobbies {
-        & > ul {
+        & ul {
           @include breakpoint($desktop-width) {
+            border-bottom: solid 2px var(--clr-3);
+            margin-bottom: var(--fll-margin);
+            padding-bottom: var(--hlf-margin);
+          }
+          @include breakpoint($large-width) {
+            border-bottom: none;
             display: grid;
-            grid-template-columns: repeat(3, 1fr);
+            grid-template-columns: 1fr 1fr;
             grid-column-gap: var(--fll-margin);
-            width: 100%;
-            & p {
-              margin-bottom: var(--qtr-margin);
+            grid-row-gap: var(--hlf-margin);
+            & li {
+              &:first-of-type {
+                grid-column: 1;
+                grid-row: 1/3;
+              }
             }
           }
         }
