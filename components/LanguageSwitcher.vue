abea48b77fbbf035cd446780151cf5a70412a500
marc <fairbrother.marc@gmail.com>
2020-10-01 15:39:57 +0200

    Add hreflang tag attributes to language switcher link and don't show link to current active language

--- components/LanguageSwitcher.vue
+++ components/LanguageSwitcher.vue
@@ -1,7 +1,27 @@
 <template>
   <ul>
-    <li><nuxt-link :to="`/${currentPath.en.slug}`">EN</nuxt-link></li>
-    <li><nuxt-link :to="`/fr/${currentPath.fr.slug}`">FR</nuxt-link></li>
+    <li>
+      <nuxt-link
+        v-if="currentLocale === 'fr'"
+        :to="`/${currentPath.en.slug}`"
+        rel="alternate"
+        hreflang="en"
+      >
+        EN
+      </nuxt-link>
+      <span v-else>EN</span>
+    </li>
+    <li>
+      <nuxt-link
+        v-if="currentLocale === 'en'"
+        :to="`/fr/${currentPath.fr.slug}`"
+        rel="alternate"
+        hreflang="fr"
+      >
+        FR
+      </nuxt-link>
+      <span v-else>FR</span>
+    </li>
   </ul>
 </template>
 
@@ -12,6 +32,10 @@ export default {
     // get the current route slugs from global state
     currentPath() {
       return this.$store.state.i18n.routeParams;
+    },
+    // get the current locale code from nuxt-i18n
+    currentLocale() {
+      return this.$i18n.locale;
     },
   },
 };
