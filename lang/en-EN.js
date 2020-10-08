fdb8454983919c9d57c4eb46663b10596af7b7b2
marc <fairbrother.marc@gmail.com>
2020-10-08 16:27:59 +0200

    Add home page text

--- lang/en-EN.js
+++ lang/en-EN.js
@@ -18,6 +18,53 @@ export default {
     toLight: 'Switch to light theme',
     toDark: 'Switch to dark theme'
   },
+  index: {
+    heading: {
+      title: '<span>My name is</span> <br>Marc Fairbrother',
+      description:
+        "I'm a UX-plorer who designs and develops user-centric digital experiences.",
+      cta: 'TL;DR'
+    },
+    skillset: {
+      title: 'In a Nutshell',
+      categories: {
+        design: {
+          title: 'Web Design',
+          bullets: {
+            ux: 'Web & Mobile UX',
+            visual: 'Visual Design',
+            a11y: 'Accessibility Driven'
+          }
+        },
+        development: {
+          title: 'Development',
+          bullets: {
+            html: 'Advanced HTML & CSS',
+            js: 'Strong JS Fundamentals',
+            tools: 'Git Basics'
+          }
+        },
+        management: {
+          title: 'Management',
+          bullets: {
+            organisation: 'Project Organisation',
+            prioritisation: 'Sense of Priorities',
+            enablement: 'Team Enabler'
+          }
+        }
+      },
+      cta: 'Learn More About Me'
+    },
+    bio: {
+      title: 'Web Worker',
+      text: [
+        'In 2014, after working as webmaster and bookseller for an English-language bookshop in Paris, I joined a startup which runs multiple VOD services.',
+        'With the help of the development team I managed technological decisions, made sure that projects were moving in the right direction, and helped witth design and development work while overseeing the launch of several new applications and services.',
+        'In my free time I enjoy firing up personal projects to learn new tools and concepts, and for the fun of writing code.'
+      ],
+      cta: 'Check Out My Projects'
+    }
+  },
   about: {
     meta: {
       slug: 'about-me'
--- lang/fr-FR.js
+++ lang/fr-FR.js
@@ -18,6 +18,53 @@ export default {
     toLight: 'Activer le mode jour',
     toDark: 'Activer le mode nuit'
   },
+  index: {
+    heading: {
+      title: "<span>Je m'appelle</span> <br>Marc Fairbrother",
+      description:
+        "Je conçois et développe des expériences numériques centrées sur l'expérience utilisateur.",
+      cta: 'En Bref'
+    },
+    skillset: {
+      title: 'In a Nutshell',
+      categories: {
+        design: {
+          title: 'Design',
+          bullets: {
+            ux: 'Ergonomie web & mobile',
+            visual: 'Conception visuelle',
+            a11y: "Passionné d'accessibilité"
+          }
+        },
+        development: {
+          title: 'Développement',
+          bullets: {
+            html: 'HTML & CSS avancé',
+            js: 'JS intermédiaire',
+            tools: 'Usage basique de Git'
+          }
+        },
+        management: {
+          title: 'Management',
+          bullets: {
+            organisation: 'Organisation de projets',
+            prioritisation: 'Sens des priorités',
+            enablement: "Coaching d'équipe"
+          }
+        }
+      },
+      cta: 'Mon Parcours'
+    },
+    bio: {
+      title: 'Culture Web',
+      text: [
+        "Après avoir travaillé en tant que webmaster et libraire pour une librairie anglophone située à Paris, j'ai rejoint en 2014 une start-up éditrice de plusieurs services de VOD.",
+        "Avec l'aide des développeurs, j'ai géré les choix technologiques et je me suis assuré que les projets avançaient constamment, participant au travail de design et de développement tout en supervisant le lancement de plusieurs nouveaux services et applications.",
+        "J'aime développer des projets personnels sur mon temps libre pour apprendre de nouveaux outils et concepts, et pour le simple plaisir de coder."
+      ],
+      cta: 'Mes Projets'
+    }
+  },
   about: {
     meta: {
       slug: 'presentation'
