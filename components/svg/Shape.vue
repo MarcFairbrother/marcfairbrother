3d70c6544e2334cc6616895e5326c684141766a5
marc <fairbrother.marc@gmail.com>
2020-10-09 15:06:05 +0200

    Improve svg animation on page load

--- components/svg/Shape.vue
+++ components/svg/Shape.vue
@@ -1,16 +1,15 @@
 <template>
   <svg xmlns="http://www.w3.org/2000/svg" id="shape" viewBox="0 0 100 100">
     <path
-      d="M20 75 70 30 75 60 30 20 32 80 90 85 10 35 90 20 60 10 50 95 90 55 5 60 15 15"
+      d="M20 75 70 30 75 60 30 20 32 80 90 85 10 35 90 20 60 10 50 95 90 55 5 60 15 15 20 75"
       id="path"
       fill="none"
-      stroke-width="3.96875"
       stroke-linejoin="round"
     >
       <animate
         attributeName="d"
         id="animation"
-        values="M20 75 70 30 75 60 30 20 32 80 90 85 10 35 90 20 60 10 50 95 90 55 5 60 15 15"
+        values="M20 75 70 30 75 60 30 20 32 80 90 85 10 35 90 20 60 10 50 95 90 55 5 60 15 15 20 75"
         dur="3s"
         begin="indefinite"
       />
