c431d1ca455280796f885a66f0e40b3eff650111
marc <fairbrother.marc@gmail.com>
2020-10-14 18:10:48 +0200

    Adapt Page Heading component for tablet screens

--- components/PageHeading.vue
+++ components/PageHeading.vue
@@ -16,14 +16,24 @@ export default {
 <style lang="scss" scoped>
 .header {
   padding: 120px 15px 45px;
+  @include breakpoint($tablet-width) {
+    padding: 180px 30px 90px;
+  }
   & > h2 {
     font-size: 1.5rem;
     margin-bottom: 15px;
+    @include breakpoint($tablet-width) {
+      font-size: 2rem;
+      margin-bottom: 30px;
+    }
   }
   & > p {
     font-size: 1rem;
     line-height: 1.45;
     margin-bottom: 15px;
+    @include breakpoint($tablet-width) {
+      font-size: 1.4rem;
+    }
   }
 }
 </style>
