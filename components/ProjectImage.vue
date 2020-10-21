b189eacaee801f93128ead808cbef214614089c2
marc <fairbrother.marc@gmail.com>
2020-10-21 16:35:32 +0200

    Create ProjectImage component

---
+++ components/ProjectImage.vue
@@ -0,0 +1,20 @@
+<template>
+  <img :src="imgUrl" :alt="imgAlt" class="screenshot" />
+</template>
+
+<script>
+export default {
+  props: ['imgUrl', 'imgAlt']
+};
+</script>
+
+<style lang="scss" scoped>
+.screenshot {
+  border: solid 1px var(--altBg);
+  border-radius: 4px;
+  display: inline-block;
+  height: auto;
+  margin: -30px 0 30px;
+  width: 100%;
+}
+</style>
